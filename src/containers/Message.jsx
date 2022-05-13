fd44963660dcd74a824aafcc5fda5dce6e6ab75a
Jason McCandless <jhmccandless1105@gmail.com>
2022-05-12 23:22:31 -0500

    feat: adding component to see messages

---
+++ src/containers/Message.jsx
@@ -0,0 +1,27 @@
+import MessagesUI from "../components/MessagesUI";
+import { connect } from "react-redux";
+import { deleteMessageAction, fetchAPIDataAction } from "../action";
+
+function mapStateToProps(state) {
+  return {
+    messages: state.messages,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    deleteMessage: function (data) {
+      dispatch(deleteMessageAction(data));
+    },
+    fetchAPI: function (data) {
+      dispatch(fetchAPIDataAction(data));
+    },
+  };
+}
+
+const connectedMessagesUI = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MessagesUI);
+
+export default connectedMessagesUI;
