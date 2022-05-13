deb0a1c4c35a4c9762d434bf686a2a43cc593efe
Jason McCandless <jhmccandless1105@gmail.com>
2022-05-13 14:10:54 -0500

    feat: sql code finished for calls to the backend

--- server/index.js
+++ server/index.js
@@ -10,6 +10,36 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.get("/", async (req, res) => {
+  const results = await db.query(`SELECT * FROM messages`);
+  res.send(results);
+  //   console.log(results);
+});
+
+app.post("/messages_db", async (req, res) => {
+  const messageInfo = req.body.messageDetails;
+  console.log(messageInfo[0]);
+  let messageTitle, messageBody;
+  messageTitle = messageInfo[0];
+  messageBody = messageInfo[1];
+  date = new Date().toISOString().slice(0, 19).replace("T", " ");
+  db.none(
+    `INSERT INTO messages (deleted, message_title, message_body, created_on)
+  VALUES (
+    'true',
+    '${messageTitle}',
+    '${messageBody}',
+    '${date}')`
+  );
+});
+
+app.delete("/messages_delete:id", async (req, res) => {
+  res.send("deleting");
+  const deleteInfo = req.params.id.slice(1);
+  console.log(deleteInfo);
+  db.none(`DELETE FROM messages WHERE messages.id = ${deleteInfo}`);
+});
+
 const PORT = process.env.PORT || 3785;
 
 app.set("port", PORT);
